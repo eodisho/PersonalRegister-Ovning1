2d632cecf18665c4a4dfd2d429de22be9992b64e
Edwar Odisho <edwar_odisho@hotmail.com>
2021-01-20 16:27:38 +0100

    sent by Edwar today

---
+++ PersonalRegister-Ovning1/PersonalRegister.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister_Ovning1
+{
+    class PersonalRegister
+    {
+     public void AddEmployeeName( string name)
+        {
+            employeeName.Add(name);
+        }
+        public void AddEmployeeSalary(float salary)
+        {
+            employeeSalary.Add(salary);
+        }
+
+        List<string> employeeName = new List<string>();
+        List<float> employeeSalary = new List<float>();
+
+    }
+
+
+}
---
+++ PersonalRegister-Ovning1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PersonalRegister_Ovning1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonalRegister register = new PersonalRegister();
+            register.AddEmployeeName("Joe");
+            register.AddEmployeeSalary(200.0f);
+   
+        }
+    }
+}
